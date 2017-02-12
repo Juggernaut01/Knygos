c7caca2a18075067af59e5d6d75fe3e48b27dd36
Juggernaut01 <erikas.ustila@gmail.com>
2017-02-12 17:30:46 +0200

    Update antrasis_puslapis.php

--- antrasis_puslapis.php
+++ antrasis_puslapis.php
@@ -24,7 +24,7 @@ function goBack() {
 $sql = "SELECT Title, Year, Author, Genre FROM knygos WHERE Title='$row';";
 $result = mysqli_query($conn, $sql);
 
-echo "<center><h1>Information the about book:</h1></center>";
+echo "<center><h1>Information about the book:</h1></center>";
 
 if (mysqli_num_rows($result) > 0) {
    
